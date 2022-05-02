a571d3e8e94c9495d9fd57947e4a6556223cc836
hamideh <hamideh.ht@gmail.com>
2022-05-03 02:31:15 +0430

    login UI completed

--- src/components/NavComponents/Login.js
+++ src/components/NavComponents/Login.js
@@ -3,29 +3,29 @@ import React from "react";
 function Login() {
 return (
 
-<div class="w-full min-h-screen bg-gray-50 flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
-  <div class="w-full sm:max-w-md p-5 mx-auto">
-    <h2 class="mb-12 text-center text-2xl font-extrabold">ورود به پنل کاربری</h2>
+<div className="w-full min-h-screen bg-gray-50 flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
+  <div className="w-full sm:max-w-md p-5 mx-auto">
+    <h2 className="mb-12 text-center text-2xl font-extrabold">ورود به پنل کاربری</h2>
     <form>
-      <div class="mb-4">
-        <label class="block mb-1" for="email">ایمیل</label>
-        <input id="email" type="text" name="email" class="py-2 px-3 border border-gray-300 focus:border-red-300 focus:outline-none focus:ring focus:ring-red-200 focus:ring-opacity-50 rounded-md shadow-sm disabled:bg-gray-100 mt-1 block w-full" />
+      <div className="mb-4">
+        <label className="block mb-1" htmlFor="email">ایمیل</label>
+        <input id="email" type="text" name="email" className="py-2 px-3 border border-gray-300 focus:border-red-300 focus:outline-none focus:ring focus:ring-red-200 focus:ring-opacity-50 rounded-md shadow-sm disabled:bg-gray-100 mt-1 block w-full" />
       </div>
-      <div class="mb-4">
-        <label class="block mb-1" for="password">پسورد</label>
-        <input id="password" type="password" name="password" class="py-2 px-3 border border-gray-300 focus:border-red-300 focus:outline-none focus:ring focus:ring-red-200 focus:ring-opacity-50 rounded-md shadow-sm disabled:bg-gray-100 mt-1 block w-full" />
+      <div className="mb-4">
+        <label className="block mb-1" htmlFor="password">پسورد</label>
+        <input id="password" type="password" name="password" className="py-2 px-3 border border-gray-300 focus:border-red-300 focus:outline-none focus:ring focus:ring-red-200 focus:ring-opacity-50 rounded-md shadow-sm disabled:bg-gray-100 mt-1 block w-full" />
       </div>
-      <div class="mt-6 flex items-center justify-between">
-        <div class="flex items-center">
-          <input id="remember_me" type="checkbox" class="border border-gray-300 text-red-600 shadow-sm focus:border-red-300 focus:ring focus:ring-red-200 focus:ring-opacity-50" />
-          <label for="remember_me" class="ml-2 block text-sm leading-5 text-gray-900">به خاطر بسپار </label>
+      <div className="mt-6 flex items-center justify-between">
+        <div className="flex items-center">
+          <input id="remember_me" type="checkbox" className="border border-gray-300 text-red-600 shadow-sm focus:border-red-300 focus:ring focus:ring-red-200 focus:ring-opacity-50" />
+          <label htmlFor="remember_me" className="ml-2 block text-sm leading-5 text-gray-900">به خاطر بسپار </label>
         </div>
-        <a href="#" class="text-sm">رمز عبور را فراموش کرده ام</a>
+        <a href="#" className="text-sm">رمز عبور را فراموش کرده ام</a>
       </div>
-      <div class="mt-6">
-        <button class="w-full inline-flex items-center justify-center px-4 py-2 bg-blue-400 border border-transparent rounded-md font-semibold capitalize text-white hover:bg-blue-700 active:bg-red-700 focus:outline-none focus:border-red-700 focus:ring focus:ring-red-200 disabled:opacity-25 transition">ورود</button>
+      <div className="mt-6">
+        <button className="w-full inline-flex items-center justify-center px-4 py-2 bg-blue-400 border border-transparent rounded-md font-semibold capitalize text-white hover:bg-blue-700 active:bg-red-700 focus:outline-none focus:border-red-700 focus:ring focus:ring-red-200 disabled:opacity-25 transition">ورود</button>
       </div>
-      <div class="mt-6 text-center">
+      <div className="mt-6 text-center">
         <a href="#" >ایجاد حساب کاربری</a>
       </div>
     </form>
