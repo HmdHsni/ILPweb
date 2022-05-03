b3dc2ae5547128b595c3edd4e9e445aecf80af21
hamideh <hamideh.ht@gmail.com>
2022-05-04 02:08:55 +0430

    dynamic form

--- src/components/FormComponents/Input.js
+++ src/components/FormComponents/Input.js
@@ -3,10 +3,12 @@ const Input=(props)=>{
     let InputElement=null;
     switch(props.inputtype){
         case"input":
-        InputElement= <input className="py-2 px-3 border border-gray-300 focus:border-red-300 focus:outline-none focus:ring focus:ring-red-200 focus:ring-opacity-50 rounded-md shadow-sm disabled:bg-gray-100 mt-1 block w-full " {...props}/>
+        InputElement= <input className="py-2 px-3 border border-gray-300 focus:border-red-300 focus:outline-none focus:ring focus:ring-red-200 focus:ring-opacity-50 rounded-md shadow-sm disabled:bg-gray-100 mt-1 block w-full "
+        {...props.elementConfige} value={props.value}/>
         break
         default:
-        InputElement= <input className="py-2 px-3 border border-gray-300 focus:border-red-300  focus:outline-none focus:ring focus:ring-red-200 focus:ring-opacity-50 rounded-md shadow-sm  disabled:bg-gray-100 mt-1 block w-full " {...props}/>                                    
+        InputElement= <input className="py-2 px-3 border border-gray-300 focus:border-red-300  focus:outline-none focus:ring focus:ring-red-200 focus:ring-opacity-50 rounded-md shadow-sm  disabled:bg-gray-100 mt-1 block w-full "
+        {...props.elementConfige} value={props.value}/>                                    
     }                                                                 
     return(
         <div>{InputElement}</div>
--- src/components/NavComponents/Login.js
+++ src/components/NavComponents/Login.js
@@ -2,21 +2,62 @@
 import React from "react";
 import Button from "../FormComponents/Button";
 import Input from '../FormComponents/Input';
-function Login() {
+class Login extends React.Component {
+  state={
+    form:{
+        email:{
+          elementtype:"input",
+          elementConfige:{
+            type:"text",
+            placeholder:"ایمیل",
+                   },
+          value:"",
+        },
+        password:{
+          elementtype:"input",
+          elementConfige:{
+            type:"text",
+            placeholder:"پسورد",
+                   },
+          value:"",
+        },
+
+    }
+  }
+  render(){
+    const elementsArray=[]
+    for(let item in this.state.form){
+      elementsArray.push({
+        id:item,
+        config:this.state.form[item]
+      })
+    }
+    console.log(elementsArray);
 return (
 
 <div className="w-full min-h-screen bg-gray-50 flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
   <div className="w-full sm:max-w-md p-5 mx-auto">
     <h2 className="mb-12 text-center text-2xl font-extrabold">ورود به پنل کاربری</h2>
     <form>
-      <div className="mb-4">
+      {elementsArray.map((item)=>{
+        return(
+          <div className="mb-4">
+            <label className="block mb-1" htmlFor="email">{item.config.elementConfige.placeholder}</label>
+          <Input 
+          key={item.id}
+          elementType={item.config.elementConfige}
+          value={item.config.value}/>
+          </div>
+        )
+      })}
+      {/* <div className="mb-4">
         <label className="block mb-1" htmlFor="email">ایمیل</label>
         <Input id="email" type="text" name="email"  placeholder=" ایمیل..."/>
-      </div>
-      <div className="mb-4">
+      </div> */}
+      {/* <div className="mb-4">
         <label className="block mb-1" htmlFor="password">پسورد</label>
         <Input id="password" type="password" name="password" placeholder=" پسورد ..." />
-      </div>
+      </div> */}
       <div className="mt-6 flex items-center justify-between">
         <div className="flex items-center">
           <Input id="remember_me" type="checkbox" className="text-red-600  focus:border-red-300"  />
@@ -35,6 +76,6 @@ return (
 </div>
 
 );
-}
+}}
 
 export default Login;
